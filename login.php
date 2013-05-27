b69bea169954e62e37f7c3725896001da8284252
KulandaiKumar <kumark@xsysys.com>
2013-05-27 12:17:53 +0530

    Forgot passsword
    Forgot passsword Block added newly

--- login.php
+++ login.php
@@ -14,7 +14,17 @@ if($_REQUEST['submit']!='')
 	<div>
 	<label for="username">Username: </label>&nbsp;<input type="input" value=""></input>
 	<label for="pw">password: </label>&nbsp;<input type="password" value=""></input>
-	<input type="Submit"></input>
+	<input type="Submit" value="Submit"></input>
+	<input type="button" value="forgot password" onclick="/$('#getNewPw').css({'display':'block'})"/></input>
+	</div>
+	<div style="display:none;" id="getNewPw">
+		<label for="username">Username:</label>&nbsp;<input type="input" value=""></input>
+		<label for="npw">Enter your email: </label>&nbsp;<input name="email" id="email" type="email" value=""></input>
+		
+		if(email!=="" and email!=="null")
+		{
+		 <p>We have sent new password to your email address</p>
+		}
 	</div>
   </form>
 </body>
