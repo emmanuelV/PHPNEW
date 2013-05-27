8560b40ab7b48f5588015c576a6f3c9c9b255ba9
emmanuelV <emmanuel@xsysys.com>
2013-05-27 10:25:25 +0530

    another Table
    added another table

--- login.php
+++ login.php
@@ -43,6 +43,22 @@ if($_REQUEST['submit']!='')
 	    <td colspan="2" style="padding-left:120px;"><input type="submit" name="submit" value="Submit"></td>
 	  </tr>
 	</table>
+		<table border=0 cellpadding=3 cellspacing=3 align=center style="padding-top:120px;" valign="center">
+	 <tr>
+	    <td colspan="3" align="center"><h3>LOGIN</h3></td>
+	  </tr>
+	  <tr>
+	    <td valign="top">Username:</td>
+		<td><input type="text" name="username"></td> 
+	  </tr>
+	  <tr>
+	    <td valign="top">Password:</td>
+		<td><input type="text" name="password"></td> 
+	  </tr>
+	  <tr>
+	    <td colspan="2" style="padding-left:120px;"><input type="submit" name="submit" value="Submit"></td>
+	  </tr>
+	</table>
   </form>
 </body>
 </html>
