2e0bbcd69ad577ea32248c7094eafe30dc9d9a17
emmanuelV <emmanuel@xsysys.com>
2013-05-27 11:19:01 +0530

    new LOgin Page
    Login page edited

--- login.php
+++ login.php
@@ -11,56 +11,11 @@ if($_REQUEST['submit']!='')
 <body>
 <h3 align="center" ><?=$retString?></h3>
   <form name="loginform" method="POST" action="/home/login">
-  	<table border=0 cellpadding=3 cellspacing=3 align=center style="padding-top:120px;" valign="center">
-	 <tr>
-	    <td colspan="3" align="center"><h3>LOGIN</h3></td>
-	    <td colspan="3" align="center"><h3>LOGIN</h3></td>
-	  </tr>
-	  <tr>
-	    <td valign="top">Username:</td>
-		<td><input type="text" name="username"></td> 
-	  </tr>
-	  <tr>
-	    <td valign="top">Password:</td>
-		<td><input type="text" name="password"></td> 
-	  </tr>
-	  <tr>
-	    <td colspan="2" style="padding-left:120px;"><input type="submit" name="submit" value="Submit"></td>
-	  </tr>
-	</table>
-	<table border=0 cellpadding=3 cellspacing=3 align=center style="padding-top:120px;" valign="center">
-	 <tr>
-	    <td colspan="3" align="center"><h3>LOGIN</h3></td>
-	  </tr>
-	  <tr>
-	    <td valign="top">Username:</td>
-		<td><input type="text" name="username"></td> 
-	  </tr>
-	  <tr>
-	    <td valign="top">Password:</td>
-		<td><input type="text" name="password"></td> 
-	  </tr>
-	  <tr>
-	    <td colspan="2" style="padding-left:120px;"><input type="submit" name="submit" value="Submit"></td>
-	  </tr>
-	</table>
-		<table border=0 cellpadding=3 cellspacing=3 align=center style="padding-top:120px;" valign="center">
-	 <tr>
-	    <td colspan="3" align="center"><h3>LOGIN</h3></td>
-	  </tr>
-	  <tr>
-	    <td valign="top">Username:</td>
-		<td><input type="text" name="username"></td> 
-	  </tr>
-	  <tr>
-	    <td valign="top">Password:</td>
-		<td><input type="text" name="password"></td> 
-	  </tr>
-	  <tr>
-	    <td colspan="2" style="padding-left:120px;"><input type="submit" name="submit" value="Submit"></td>
-	  </tr>
-	</table>
-	<div>this is block 2</div>
+	<div>
+	<label for="username">Username: </label>&nbsp;<input type="input" value=""></input>
+	<label for="pw">password: </label>&nbsp;<input type="password" value=""></input>
+	<input type="Submit"></input>
+	</div>
   </form>
 </body>
 </html>
