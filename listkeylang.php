db525b0cc022bf03806641073328e2d5c22571ee
emmanuelV <emmanuel@xsysys.com>
2013-05-27 15:18:34 +0530

    Language Code

---
+++ listkeylang.php
@@ -0,0 +1,66 @@
+<?php
+
+$objLang=new Translation();
+   
+    if($_REQUEST['addlang']==1)
+	{ 
+		if($_REQUEST['createlang']=="Create"){	
+		$retString=$objLang->addTransl($_REQUEST);	
+		}
+
+		if($_REQUEST['createlang']=="Update"){
+			$retString=$objLang->updateTransl($_REQUEST);	
+		}
+	}
+	if($_REQUEST['delete']==1)
+	{ 
+	  $retString=$objLang->deleteKey($_REQUEST['id']);
+	}
+    unset($_SESSION['lang']);
+	$_SESSION['lang']=$_REQUEST['lang'];
+	if($_REQUEST['lang']=='')
+	   $_SESSION['lang']='28';
+?>
+
+<html>
+<head>
+  <script>
+    function fnsubmit()
+	{
+	 document.langform.submit();
+	 val=document.getElementById("lang").value;
+	 document.location.href="/home/listkeylang/lang/"+val;
+	}
+  </script>
+</head>
+<body align="center">
+<table align="center" width="90%">
+	<tr>
+		<td colspan="2" align="center">
+		<h3><?=$txt?></h3>
+		<h3>List Of Languages</h3></td>		
+	</tr>
+	<tr>	
+		<td align="right"><a href="/home/languagesettings/addLang/1/<?=$SegmentId?>" >Create Language</a> | <a href="/home/addlanguage">Add Language Key</a> | <a href="/home/listkeylang/">List Language Key</a></td>
+	</tr>
+    <tr>
+	   
+	   <td>Language:
+	     <?
+			$lang=$objLang->getLang();
+			echo $lang;
+		?>
+	   </td>
+    </tr>	
+	<tr>
+		<td>
+		<?
+			$langlist=$objLang->getKeyList();
+			echo $langlist;
+		?>
+		</td>
+	</tr>
+</table>
+<br><br>
+</body>
+</html>
